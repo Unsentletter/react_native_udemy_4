c135c7818ab00d42940b57331b6c8b5a6f34bb47
Tristan <tristan.southwell@hotmail.com>
2016-11-05 07:43:51 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import createStore from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: 'AIzaSyDRVxv58bbjTylR-CJHGvFW_tqW5HN8wvU',
+      authDomain: 'udemy-manager-6ec84.firebaseapp.com',
+      databaseURL: 'https://udemy-manager-6ec84.firebaseio.com',
+      storageBucket: 'udemy-manager-6ec84.appspot.com',
+      messagingSenderId: '1090422992771'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore()}>
-        <View>
-          <Text>
-            Hello!
-          </Text>
-        </View>
+      <Provider store={store}>
+        <LoginForm />
       </Provider>
     );
   }
